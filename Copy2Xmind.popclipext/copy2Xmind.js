c174933f7bcb1b71611b49be7705ea93db4b2ac4
ZackYule <leroyaum@icloud.com>
2022-08-27 12:48:25 +0800

    add a extension for copy text to xmind

---
+++ Copy2Xmind.popclipext/copy2Xmind.js
@@ -0,0 +1,8 @@
+//文本输入
+const content = popclip.input.text.replace(/[\s]/g,"").replace(/。+/g,"。").replace(/^。|。$/g,"") // 全部读取并去除空白
+
+//文本转换
+const text_out = content.replace(/。/g, '\n')
+
+//文本拷贝
+popclip.copyText(text_out);
