a2159c0bae9992834f8f897ac75e0fa4e73c7537
ZackYule <leroyaum@icloud.com>
2022-08-27 20:08:43 +0800

    fix bug

--- Copy2Xmind.popclipext/copy2Xmind.js
+++ Copy2Xmind.popclipext/copy2Xmind.js
@@ -1,5 +1,5 @@
 //文本输入
-const content = popclip.input.text.replace(/[\s]/g,"").replace(/。+/g,"。").replace(/^。|。$/g,"") // 全部读取并去除空白
+const content = popclip.input.text.replace(/[\s]/g,"").replace(/。+/g,"。").replace(/^，+|，+$/g,"").replace(/^。|。$/g,"") // 全部读取并去除空白
 
 //文本转换
 const text_out = content.replace(/。/g, '\n')
