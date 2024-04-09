b8e66bf0945d389b0f72f0e6ce36b3ede7e4112d
Miles Bennett <yuezhao@nwu.edu.cn>
2024-04-09 09:47:39 +0800

    增加copy2Xmind的分行符号

--- Copy2Xmind.popclipext/copy2Xmind.js
+++ Copy2Xmind.popclipext/copy2Xmind.js
@@ -2,7 +2,7 @@
 const content = popclip.input.text.replace(/[\s]/g,"").replace(/。+/g,"。").replace(/^，+|，+$/g,"").replace(/^。|。$/g,"") // 全部读取并去除空白
 
 //文本转换
-const text_out = content.replace(/。/g, '\n')
+const text_out = content.replace(/。/g, '\n').replace(/：/g, '\n').replace(/；/g, '\n').replace(/！/g, '\n').replace(/:/g, '\n').replace(/;/g, '\n').replace(/!/g, '\n').replace(/。/g, '\n').replace(/\n\n/g, '\n')
 
 //文本拷贝
 popclip.copyText(text_out);
